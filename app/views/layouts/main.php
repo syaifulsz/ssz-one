f69e9075d1d79960c7fb10ab6b962bfa43fb8574
Syaiful Shah Zinan <i.works@live.com>
2019-09-18 00:07:07 +0800

    Main and Debugger

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -1,22 +1,46 @@
+<?php
+
+use app\components\Asset;
+use app\components\Session;
+use app\components\Config;
+
+?>
+
 <!DOCTYPE html>
-<html lang="en" class="<?= $this->getHtmlClass() ?>">
+<html lang="en" class="<?= $this->getHtmlClass() ?> html-ssz1">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="_token" content="<?= Session::getInstance()->getToken() ?>">
+    <meta name="_envyronment" content="<?= Config::getInstance()->get( 'app.envyronment' ) ?>">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+    <link rel="stylesheet" href="<?= Asset::cacheBooster( '/assets/css/common.min.css' ) ?>">
     <?= $this->getBlockHead() ?>
     <title><?= $this->getPageTitle() ?></title>
 </head>
 <body class="<?= $this->getBodyClass() ?>">
 
-    <?= $this->getBlockBodyStart() ?>
+<?= $this->staticRender( 'common/header' ) ?>
+<?= $this->staticRender( 'common/breadcrumb' ) ?>
+<?= $this->getBlockBodyStart() ?>
+<?= $content ?? '' ?>
 
-    <?= $content ?? '' ?>
+<div class="container">
+    Rendered Templates:
+    <ul class="text-muted">
+        <?php foreach ( $this->templates as $template => $path ) : ?>
+            <li>
+                <div><?= $template ?></div>
+                <div class="small"><?= $path ?></div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
 
-    <?= $this->getBlockBodyEnd() ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="<?= Asset::cacheBooster( '/assets/js/vendors.min.js' ) ?>"></script>
+<script src="<?= Asset::cacheBooster( '/assets/js/common.min.js' ) ?>"></script>
+<?= $this->getBlockBodyEnd() ?>
 </body>
 </html>
